497b67671e892a5a29ed9b434e0056ae92d23744
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-07-07 08:02:43 +0700

    Some Change

--- client/src/components/PhoneList.js
+++ client/src/components/PhoneList.js
@@ -1,7 +1,7 @@
 import PhoneItem from "./PhoneItem"
 export default function PhoneList({ phonebooks, remove, update,containerRef}) {
     return (
-        <div ref={containerRef} style={{ height:'100vh', overflowY:"scroll" }}>
+        <div ref={containerRef} style={{ height:'250px', overflowY:"scroll" }}>
             <ul>
                 {
                     phonebooks.map((user) => (
